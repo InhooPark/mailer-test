10415056cd6144547ae4e5635a690c85179e8a07
InhooPark <park.linhoo@gmail.com>
2023-02-23 10:56:00 +0900

    first commit

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,32 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
-}
+const nodemailer = require("nodemailer");
+const transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: "id@gmail.com",
+    pass: "password",
+  },
+});
+
+const mailOptions = {
+  from: "poke@poke",
+  to: "park.linhoo@gmail.com",
+  subject: "인증 메일",
+  // html: `<p>메일의 내용(html 템플릿 형식으로 작성)</p>`,
+  // text: "템플릿 정도가 아니고 단순히 텍스트 보낼때는 해당 값으로 보내도 됨",
+  text: "인증 메일입니다.",
+};
+
+transporter.sendMail(mailOptions, (err, data) => {
+  if (err) {
+    console.error(err);
+    res.status(500).json({ status: "fail" });
+  } else {
+    res.status(200).json({ status: "success" });
+  }
+});
+
+// export default function handler(req, res) {
+//   res.status(200).json({ name: "John Doe" });
+// }
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,32 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from '@next/font/google'
-import styles from '@/styles/Home.module.css'
-
-const inter = Inter({ subsets: ['latin'] })
+import Head from "next/head";
+import emailjs from "@emailjs/browser";
+import { useRef } from "react";
 
 export default function Home() {
+  const form = useRef();
+  const btnClick = (e) => {
+    e.preventDefault();
+    const t = e.target;
+    const payload = { email: t.email.value, title: t.title.value, message: t.message.value };
+    console.log(payload);
+
+    // emailjs.sendForm(process.env.MAIL_SERVER_KEY, process.env.MAIL_TEMPLATE_KEY, payload, process.env.MAIL_PUBLIC_KEY).then(
+    //   (result) => {
+    //     console.log(result.text);
+    //   },
+    //   (error) => {
+    //     console.log(error);
+    //   }
+    // );
+    try {
+      emailjs.sendForm(process.env.MAIL_SERVER_KEY, process.env.MAIL_TEMPLATE_KEY, form.current, process.env.MAIL_PUBLIC_KEY);
+      console.log("success");
+    } catch (error) {
+      console.log(error);
+    }
+    t.reset();
+  };
+
   return (
     <>
       <Head>
@@ -14,110 +35,16 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={styles.main}>
-        <div className={styles.description}>
+      <main>
+        <form ref={form} onSubmit={btnClick}>
           <p>
-            Get started by editing&nbsp;
-            <code className={styles.code}>pages/index.js</code>
+            <input type="email" name="email" placeholder="enter your email"></input>
+            <input type="text" name="title" placeholder="enter your title"></input>
+            <textarea name="message"></textarea>
+            <input type="submit" value="전송"></input>
           </p>
-          <div>
-            <a
-              href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              By{' '}
-              <Image
-                src="/vercel.svg"
-                alt="Vercel Logo"
-                className={styles.vercelLogo}
-                width={100}
-                height={24}
-                priority
-              />
-            </a>
-          </div>
-        </div>
-
-        <div className={styles.center}>
-          <Image
-            className={styles.logo}
-            src="/next.svg"
-            alt="Next.js Logo"
-            width={180}
-            height={37}
-            priority
-          />
-          <div className={styles.thirteen}>
-            <Image
-              src="/thirteen.svg"
-              alt="13"
-              width={40}
-              height={31}
-              priority
-            />
-          </div>
-        </div>
-
-        <div className={styles.grid}>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Docs <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Find in-depth information about Next.js features and&nbsp;API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Learn <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Learn about Next.js in an interactive course with&nbsp;quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Templates <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Discover and deploy boilerplate example Next.js&nbsp;projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Deploy <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Instantly deploy your Next.js site to a shareable URL
-              with&nbsp;Vercel.
-            </p>
-          </a>
-        </div>
+        </form>
       </main>
     </>
-  )
+  );
 }
